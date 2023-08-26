58e269fcd7680ba28f5446fa16e4f0fd2f402ef0
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-08-26 16:40:16 +0400

    third Comment

--- src/Chat.js
+++ src/Chat.js
@@ -132,7 +132,7 @@ export default function Chat(props) {
 
                 {/* messages start here */}
 
-                <div id='messages' className='flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
+                <div id='messages' className='flex flex-col space-y-4 p-3 overflow-y-auto h-[500px] md:h-auto lg:h-auto xl:h-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
                 {/* old Chat */}
                 {messageCount > 10 ? 
                 <div className="text-center">
